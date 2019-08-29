1637177328b44bb5dd58ad0617477ee528e3feb9
Maciej Kwiek <maciej@isovalent.com>
2019-08-29 15:37:51 +0200

    Move BasePath to SSHMeta and Kubectl structs
    [ upstream commit 6ade55006f853443075257f3b5973b14b3b94fd0 ]
    
    BasePath was a global variable in test suite. It was moved into
    Executor interface, so that tests may access it base on the executor
    they are using.
    
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- test/k8sT/assertionHelpers.go
+++ test/k8sT/assertionHelpers.go
@@ -108,9 +108,6 @@ func ExpectCiliumPreFlightInstallReady(vm *helpers.Kubectl) {
 
 // DeployCiliumAndDNS deploys DNS and cilium into the kubernetes cluster
 func DeployCiliumAndDNS(vm *helpers.Kubectl) {
-	By("Installing DNS Deployment")
-	_ = vm.Apply(helpers.DNSDeployment())
-
 	DeployCiliumOptionsAndDNS(vm, []string{})
 }
 
@@ -123,12 +120,12 @@ func DeployCiliumOptionsAndDNS(vm *helpers.Kubectl, options []string) {
 	ExpectCiliumRunning(vm)
 
 	By("Installing DNS Deployment")
-	_ = vm.Apply(helpers.DNSDeployment())
+	_ = vm.Apply(helpers.DNSDeployment(vm.BasePath()))
 
 	switch helpers.GetCurrentIntegration() {
 	case helpers.CIIntegrationFlannel:
 		By("Installing Flannel")
-		vm.Apply(helpers.GetFilePath("../examples/kubernetes/addons/flannel/flannel.yaml"))
+		vm.Apply(vm.GetFilePath("../examples/kubernetes/addons/flannel/flannel.yaml"))
 	default:
 	}
 
